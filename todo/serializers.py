f8970f07b5264eb2cca006a8d54eb57365b1dc02
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-10-15 11:40:08 +0200

    Django Restframewokr

---
+++ todo/serializers.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ["id", 'title', 'description',
+                  'completed', 'created_at']  # ,'user']
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all().order_by('-created_at')
+    serializer_class = TodoSerializer
+    permission_classes = []  # permissions.IsAuthenticated
