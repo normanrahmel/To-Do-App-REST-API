8d489920ed06f14c387f35ef6cd3da33998298d5
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-10-15 14:15:06 +0200

    new serializers

--- todo/serializers.py
+++ todo/serializers.py
@@ -3,8 +3,16 @@ from rest_framework import serializers
 from .models import Todo
 
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username', 'email']
+
+
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Todo
         fields = ["id", 'title', 'description',
-                  'completed', 'created_at']  # ,'user']
+                  'completed', 'created_at']
