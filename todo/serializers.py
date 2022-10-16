5aae6b16cf8c08c081ee3c4d1a07e3b8e5ffa2ef
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-10-16 13:40:25 +0200

    new and create func serializers

--- todo/serializers.py
+++ todo/serializers.py
@@ -6,13 +6,17 @@ from .models import Todo
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'username', 'email']
+        # user = request.user
+        fields = ['id', 'first_name', 'last_name', 'username', 'email', ]
 
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserSerializer()
+    # user = CurrentUserDefault()
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault())
 
     class Meta:
         model = Todo
-        fields = ["id", 'title', 'description',
-                  'completed', 'created_at']
+        # user = request.user
+        fields = ['id', 'title', 'description',
+                  'created_at', 'user', 'time_passed']
--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
-
-
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from .models import Todo
 from .serializers import TodoSerializer
+from telnetlib import STATUS
+from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework import viewsets
+from django.core import serializers
 
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all().order_by('-created_at')
     serializer_class = TodoSerializer
     permission_classes = []  # permissions.IsAuthenticated
+
+    def create(self, request):
+        todo = Todo.objects.create(title=request.POST.get('title', ''),
+                                   description=request.POST.get(
+                                       'description', ''),
+                                   user=request.user,
+                                   )
+        serialized_obj = serializers.serialize('json', [todo, ])
+        return HttpResponse(serialized_obj, content_type='application/json')
